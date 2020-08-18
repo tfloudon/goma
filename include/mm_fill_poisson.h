3ded84145d1dbb301694ab949aee5d8ba9811a19
Weston Ortiz <weston@wortiz.com>
2020-08-18 11:48:02 -0600

    add missing poisson files

---
+++ include/mm_fill_poisson.h
@@ -0,0 +1,8 @@
+#ifndef GOMA_MM_FILL_POISSON
+#define GOMA_MM_FILL_POISSON
+
+#include "goma.h"
+
+extern int assemble_poisson(void);
+
+#endif // GOMA_MM_FILL_POISSON
---
+++ src/mm_fill_poisson.c
@@ -0,0 +1,68 @@
+#include "goma.h"
+#include "mm_as.h"
+#include "mm_as_const.h"
+#include "mm_as_structs.h"
+#include <math.h>
+
+#include "mm_fill_poisson.h"
+#include "rf_fem_const.h"
+
+int assemble_poisson() {
+
+  int eqn = R_POISSON;
+
+  // We shouldn't call this function
+  // if poisson not enabled
+  if (!pd->e[eqn]) {
+    return -1;
+  }
+
+  dbl g = - 10 * exp(-(SQUARE((fv->x[0] - 0.5)) + SQUARE((fv->x[1]-0.5))) / 0.02);
+
+  if (af->Assemble_Residual) {
+    int peqn = upd->ep[eqn];
+    for (int i = 0; i < ei->dof[eqn]; i++) {
+      dbl diffusion = 0;
+      if (pd->etm[eqn][LOG2_DIFFUSION]) {
+        for (int a = 0; a < pd->Num_Dim; a++) {
+          diffusion += bf[eqn]->grad_phi[i][a] * fv->grad_u[a];
+        }
+      }
+      diffusion *= -fv->wt * bf[eqn]->detJ * fv->h3;
+      diffusion *= pd->etm[eqn][LOG2_DIFFUSION];
+
+      dbl source = 0;
+      if (pd->etm[eqn][LOG2_SOURCE]) {
+        source += g * bf[eqn]->phi[i];
+      }
+      source *= -fv->wt * bf[eqn]->detJ * fv->h3;
+      source *= pd->etm[eqn][LOG2_SOURCE];
+
+      lec->R[LEC_R_INDEX(peqn, i)] += diffusion + source;
+    }
+  }
+  
+  if (af->Assemble_Jacobian) {
+    int peqn = upd->ep[eqn];
+    for (int i = 0; i < ei->dof[eqn]; i++) {
+      int var = POISSON;
+      for (int j = 0; j < ei->dof[eqn]; j++) {
+        int pvar = upd->vp[var];
+        dbl diffusion = 0;
+        if (pd->etm[eqn][LOG2_DIFFUSION]) {
+          for (int a = 0; a < pd->Num_Dim; a++) {
+            diffusion += bf[eqn]->grad_phi[i][a] * bf[var]->grad_phi[j][a];
+          }
+        }
+        diffusion *= -fv->wt * bf[eqn]->detJ * fv->h3;
+        diffusion *= pd->etm[eqn][LOG2_DIFFUSION];
+
+        dbl source = 0;
+
+        lec->J[LEC_J_INDEX(peqn, pvar, i, j)] += diffusion + source;
+      }
+    }
+  }
+
+  return 0;
+}
